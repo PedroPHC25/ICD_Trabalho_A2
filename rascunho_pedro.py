87a40e7c7450d45475af6388380bc50a3483e273
Pedro H. Coterli <pedrocoterli@gmail.com>
2023-06-26 12:17:12 -0300

    Adicionei o rascunho Pedro

---
+++ rascunho_pedro.py
@@ -0,0 +1,8 @@
+import pandas as pd
+from bokeh.plotting import figure
+from bokeh.io import output_file, save, show
+from bokeh.models import ColumnDataSource
+
+# Geração de eletricidade por petróleo no mundo por ano (gráfico de linha)
+# Relação entre renda per capita e produção de petróleo em 2019 (scatter)
+# ???

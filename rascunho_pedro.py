636fa1621f0cabeef128958bd4833a73c274af1d
Pedro H. Coterli <pedrocoterli@gmail.com>
2023-06-26 12:31:33 -0300

    Gráfico "PIB x Consumo" não está funcionando

--- rascunho_pedro.py
+++ rascunho_pedro.py
@@ -10,11 +10,20 @@ from bokeh.models import ColumnDataSource
 data_oil = pd.read_csv("World Energy Consumption.csv")
 
 data_oil_world_year = ColumnDataSource(data_oil[data_oil["country"] == "World"])
+data_oil_gdp_production = ColumnDataSource(data_oil[data_oil["year"] == 2019])
 
-output_file("rascunho_pedro.html")
+output_file("rascunho_pedro_1.html")
 
 graph_world_year = figure()
 
 graph_world_year.line(x = "year", y = "oil_production", source = data_oil_world_year)
 
-show(graph_world_year)
+save(graph_world_year)
+
+output_file("rascunho_pedro_2.html")
+
+graph_gdp_production = figure()
+
+graph_gdp_production.circle(x = "gdp", y = "oil_consumption", source = data_oil_gdp_production)
+
+show(graph_gdp_production)

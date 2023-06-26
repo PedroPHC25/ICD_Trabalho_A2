9b774eb072e061819d73fe8bd663db9d4ee71167
Pedro H. Coterli <pedrocoterli@gmail.com>
2023-06-26 16:15:48 -0300

    Criei o gridplot dos 9 maiores países do petróleo

--- rascunho_pedro.py
+++ rascunho_pedro.py
@@ -32,7 +32,7 @@ graph_world_year = figure()
 graph_world_year.line(x = "year", y = "oil_production", source = cds_oil_world, line_color = "green")
 graph_world_year.line(x = "year", y = "oil_consumption", source = cds_oil_world, line_color = "red")
 
-save(graph_world_year)
+# save(graph_world_year)
 
 
 output_file("rascunho_pedro_2.html")
@@ -41,4 +41,40 @@ graph_gdp_production = figure()
 
 graph_gdp_production.circle(x = "oil_production", y = "oil_consumption", size = "population", source = cds_oil_2019)
 
-save(graph_gdp_production)
+# save(graph_gdp_production)
+
+
+output_file("rascunho_pedro_3.html")
+
+graph_united_states = figure()
+graph_united_states.vbar(x = "year", top = "oil_prod_change_twh", source = cds_oil_best_countries["United States"])
+
+graph_russia = figure()
+graph_russia.vbar(x = "year", top = "oil_prod_change_twh", source = cds_oil_best_countries["Russia"])
+
+graph_saudi_arabia = figure()
+graph_saudi_arabia.vbar(x = "year", top = "oil_prod_change_twh", source = cds_oil_best_countries["Saudi Arabia"])
+
+graph_canada = figure()
+graph_canada.vbar(x = "year", top = "oil_prod_change_twh", source = cds_oil_best_countries["Canada"])
+
+graph_iraq = figure()
+graph_iraq.vbar(x = "year", top = "oil_prod_change_twh", source = cds_oil_best_countries["Iraq"])
+
+graph_china = figure()
+graph_china.vbar(x = "year", top = "oil_prod_change_twh", source = cds_oil_best_countries["China"])
+
+graph_iran = figure()
+graph_iran.vbar(x = "year", top = "oil_prod_change_twh", source = cds_oil_best_countries["Iran"])
+
+graph_brazil = figure()
+graph_brazil.vbar(x = "year", top = "oil_prod_change_twh", source = cds_oil_best_countries["Brazil"])
+
+graph_united_arab_emirates = figure()
+graph_united_arab_emirates.vbar(x = "year", top = "oil_prod_change_twh", source = cds_oil_best_countries["United Arab Emirates"])
+
+grid = gridplot([[graph_united_states, graph_russia, graph_saudi_arabia],
+                 [graph_canada, graph_iraq, graph_china],
+                 [graph_iran, graph_brazil, graph_united_arab_emirates]])
+
+show(grid)

2fca72613a9bae968ccf1217399293ce77d8f6d5
Pedro H. Coterli <pedrocoterli@gmail.com>
2023-06-26 12:19:01 -0300

    Gerei gráfico "Ano x Petróleo Mundial"

--- rascunho_pedro.py
+++ rascunho_pedro.py
@@ -6,3 +6,15 @@ from bokeh.models import ColumnDataSource
 # Geração de eletricidade por petróleo no mundo por ano (gráfico de linha)
 # Relação entre renda per capita e produção de petróleo em 2019 (scatter)
 # ???
+
+data_oil = pd.read_csv("World Energy Consumption.csv")
+
+data_oil_world_year = ColumnDataSource(data_oil[data_oil["country"] == "World"])
+
+output_file("rascunho_pedro.html")
+
+graph_world_year = figure()
+
+graph_world_year.line(x = "year", y = "oil_production", source = data_oil_world_year)
+
+show(graph_world_year)

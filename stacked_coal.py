16443e3ee030f8b6e90b306151419b1ea748472f
Mariana Fernandes <rochamarianafernandes@gmail.com>
2023-06-28 15:11:28 -0300

    Filtrando só os países desejados do dataframe

--- stacked_coal.py
+++ stacked_coal.py
@@ -1,6 +1,8 @@
 from bokeh.models import ColumnDataSource
+from bokeh.plotting import figure, output_file, show
 from bokeh.palettes import GnBu3, OrRd3
 from bokeh.plotting import figure, show
+import pandas as pd
 
 # Gráfico de barras empilhadas 
 
@@ -12,9 +14,13 @@ data = pd.read_csv("World Energy Consumption.csv")
 # Categoria de cada parte da barra empilhada
 coal_products = ["coal_electricity","coal_production","coal_consumption"]
 
-stacked_countries = data[data["year"] == 1965]
+stacked_countries = data[data["year"] == 2005]
+stacked_countries = stacked_countries[["country", "coal_electricity","coal_production","coal_consumption"]]
 
-countries = ["Ukraine", "Vietnam", "United States", "United Kingdom", "Turkey", "Thailand", "Spain", "South Korea", "Russia", "Poland", "New Zealand", "Mexico", "Japan", "India", "Germany", "Canada", "Brazil"]
+# Países que vão estar no gráfico
+countries = ["Ukraine","Vietnam", "United States", "United Kingdom", "Turkey", "Thailand", "Spain", "South Korea", "Russia", "Poland", "New Zealand", "Mexico", "Japan", "India", "Germany", "Canada", "Brazil"]
 
-for country in countries:
-    stacked_countries = data[data["countries"] == country]
+stacked_countries.set_index("country", inplace = True)
+stacked_countries = stacked_countries.loc[countries]
+
+print(stacked_countries)

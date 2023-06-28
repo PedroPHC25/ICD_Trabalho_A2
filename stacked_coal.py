73f9385844a2e90239b248bc01d808421d725eb5
Mariana Fernandes <rochamarianafernandes@gmail.com>
2023-06-28 15:24:02 -0300

    Gráfico de barras horizontal na cor verde

--- stacked_coal.py
+++ stacked_coal.py
@@ -20,7 +20,25 @@ stacked_countries = stacked_countries[["country", "coal_electricity","coal_produ
 # Países que vão estar no gráfico
 countries = ["Ukraine","Vietnam", "United States", "United Kingdom", "Turkey", "Thailand", "Spain", "South Korea", "Russia", "Poland", "New Zealand", "Mexico", "Japan", "India", "Germany", "Canada", "Brazil"]
 
+# Colocando no dataframe só so países da lista acima
 stacked_countries.set_index("country", inplace = True)
 stacked_countries = stacked_countries.loc[countries]
+stacked_countries.reset_index(inplace= True)
 
-print(stacked_countries)
+# Fazendo o gráfico de barras empilhadas horizontalmente
+
+source = ColumnDataSource(stacked_countries)
+cols = ['#00ff00', '#009900', '#00cc99']
+
+coal_stacked = figure(y_range = stacked_countries["country"] ,
+             height=500)
+coal_stacked.hbar_stack(coal_products,
+               y="country",
+               source=source,
+               color=cols,
+               height=0.5)
+  
+# Display Stack Graph
+coal_stacked.legend.orientation = "horizontal"
+coal_stacked.legend.location = "bottom"
+show(coal_stacked)

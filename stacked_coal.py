1b2d3359e6fdeb477a22f98813ea464595b33575
Mariana Fernandes <rochamarianafernandes@gmail.com>
2023-06-28 16:11:02 -0300

    Adicionei o hover nas barras empilhadas

--- stacked_coal.py
+++ stacked_coal.py
@@ -14,7 +14,7 @@ data = pd.read_csv("World Energy Consumption.csv")
 # Categoria de cada parte da barra empilhada
 coal_products = ["coal_electricity","coal_production","coal_consumption"]
 
-stacked_countries = data[data["year"] == 2005]
+stacked_countries = data[data["year"] == 1995]
 stacked_countries = stacked_countries[["country", "coal_electricity","coal_production","coal_consumption"]]
 
 # Países que vão estar no gráfico
@@ -31,12 +31,22 @@ source = ColumnDataSource(stacked_countries)
 cols = ['#00ff00', '#009900', '#00cc99']
 
 coal_stacked = figure(y_range = stacked_countries["country"] ,
-             height=500)
+             height=500,
+             tools = "pan, wheel_zoom, reset, hover, save",
+             tooltips = [("Geração de eletricidade (TWh)", "@coal_electricity{1,11}"),
+                         ("Produção de energia(TWh)", "@coal_production{1,11}"),
+                         ("Consumo de energia (TWh)", "@coal_consumption{1,11}")])
 coal_stacked.hbar_stack(coal_products,
                y="country",
                source=source,
                color=cols,
-               height=0.5)
+               height=0.5,
+               legend_label=coal_products)
+
+coal_stacked.ygrid.grid_line_color = None
+coal_stacked.legend.location = "top_right"
+coal_stacked.axis.minor_tick_line_color = None
+coal_stacked.outline_line_color = None
   
 # Display Stack Graph
 coal_stacked.legend.orientation = "horizontal"

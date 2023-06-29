1082c2c22acb617f2ed8b5ae318a0bbdbeb2c892
Eduarda Mesquita <eduardammag@gmail.com>
2023-06-29 20:09:37 -0300

    alterei a main com wind

--- main.py
+++ main.py
@@ -8,6 +8,13 @@ from oil_line_production_year import graph_best_regions
 from oil_scatter_population_consumption import graph_pop_consumption
 from oil_grid_variation_year import oil_grid
 
+# Pegando os textos e os gráficos dos arquivos de energia eólica
+from wind_text import wind_text_0, wind_text_1, wind_text_2, wind_text_3, wind_text_4
+from wind_bar_top_countries import wind_bar_top_countries_graph
+from wind_grid_comparison_six_countries import wind_grid_comparison_graph
+from wind_grid_scatter_plot import wind_grid_scatter_plot
+from wind_line_brazil_evolution import wind_line_brazil_evolution_graph
+
 # Gerando a página sobre petróleo
 output_file("html_pages/oil.html")
 
@@ -18,3 +25,16 @@ save(column(oil_text_1,
             oil_text_3, 
             oil_grid, 
             oil_text_4))
+
+# Gerando a página sobre energia eólica
+output_file("html_pages/wind.html")
+
+save(column(wind_text_0, 
+            wind_grid_comparison_graph,
+            wind_text_1,
+            wind_bar_top_countries_graph, 
+            wind_text_2, 
+            wind_grid_scatter_plot, 
+            wind_text_3, 
+            wind_line_brazil_evolution_graph, 
+            wind_text_4))

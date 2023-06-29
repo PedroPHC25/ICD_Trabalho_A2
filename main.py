428a97aac029ca497ed0de9d263072703971efd5
Pedro H. Coterli <pedrocoterli@gmail.com>
2023-06-29 18:51:25 -0300

    Renomeei meus arquivos

--- main.py
+++ main.py
@@ -4,9 +4,9 @@ from bokeh.layouts import column
 
 # Pegando os textos e os gráficos dos arquivos de petróleo
 from oil_text import oil_text_1, oil_text_2, oil_text_3, oil_text_4
-from oil_production_year import graph_best_regions
-from oil_population_consumption import graph_pop_consumption
-from oil_variation_year import oil_grid
+from oil_line_production_year import graph_best_regions
+from oil_scatter_population_consumption import graph_pop_consumption
+from oil_grid_variation_year import oil_grid
 
 # Gerando a página sobre petróleo
 output_file("html_pages/oil.html")
